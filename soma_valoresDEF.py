c3b43ab5950d9c380a10a2b2acc26e8a08216d2e
Henrique Ribeiro <71899443+henriqueerss@users.noreply.github.com>
2024-10-14 12:07:39 -0300

    Add files via upload

---
+++ soma_valoresDEF.py
@@ -0,0 +1,13 @@
+def soma(x, y, z):
+    return x + y + z
+print(soma(10, 20, 30))
+print(soma(-20, 100, 50))
+
+def prod(x, y, z):
+    return x * y * z
+print(prod(10, 20, 30))
+print(prod(40, 100, 5))
+
+def nome():
+    return "HENRIQUE"
+print(nome())
